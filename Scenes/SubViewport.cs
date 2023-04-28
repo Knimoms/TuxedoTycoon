410cd784c180c8f2d5eff5157fdd96aef01569c3
Timon Albrecht <timon-albrecht@outlook.de>
2023-04-28 22:53:23 +0200

    changed sizes, added Viewport to restaurant

---
+++ Scenes/SubViewport.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class SubViewport : Godot.SubViewport
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{		
+		Size = (Vector2I)GetNode<Label>("Label").Size;
+	} 
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{	
+		Size = (Vector2I)GetNode<Label>("Label").Size;
+	}
+}
